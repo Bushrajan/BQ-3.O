987f71cb39b53fbe7a89864bed00a949213911e1
bushrajan <darkprince67890@gmail.com>
2024-06-11 17:20:57 +0500

    updates

--- JS/table.js
+++ JS/table.js
@@ -1,17 +1,16 @@
 
 function run() {
-    
-    document.getElementById("output").innerHTML= "";
-    let initial = Number(document.getElementById("initial").value);
+
+    let number = Number(document.getElementById("number").value);
     let range = Number(document.getElementById("range").value);
 
     for (let a = 1; a <= range; a++) {
-        document.getElementById("output").innerHTML += "<p>" + initial + " x " + a + " = " + initial * a + "<p>", "<br>";
+        document.getElementById("result").innerHTML += "<p>" + number + " x " + a + " = " + number * a + "<p>", "<br>";
     }
 }
 
 function clearOutput() {
-    document.getElementById("output").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
     location.reload();
 }
 
